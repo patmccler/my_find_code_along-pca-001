e07455d325bc2c6a333658270f6549f29ae0b5e9
Pat McClernan <patmccler@gmail.com>
2020-04-30 10:06:22 -0400

    solved, longer than given example

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,16 @@
 require 'pry'
 
 def my_find(collection)
+  i = 0
+  return_val = nil
+  while i < collection.size
+    if yield(collection[i])
+      return_val = collection[i]
+      break
+    end
+   # return collection[i] if yield(collection[i])
 
+    i += 1
+  end
+  return_val
 end
